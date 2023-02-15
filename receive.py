e243cc378ce89ec601544fa57e784c0a02a7cf56
lareinayden <114629329+lareinayden@users.noreply.github.com>
2023-02-15 13:20:14 -0500

    Add files via upload

--- receive.py
+++ receive.py
@@ -1,7 +1,7 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("192.168.10.101", 2390))
+sock.bind(("192.168.10.102", 2390))
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print("received message: %s" % data)
